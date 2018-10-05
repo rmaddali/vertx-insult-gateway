84440d9c8bacdf0a4b94ff002ee256002ac58c72
Ram Maddali <rmaddali@rams-mbp-2.lan>
2018-10-05 18:34:02 -0400

    second

--- src/main/java/io/vertx/starter/InsultGatewayVerticle.java
+++ src/main/java/io/vertx/starter/InsultGatewayVerticle.java
@@ -2,31 +2,76 @@ package io.vertx.starter;
 
 import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
 
-import io.vertx.config.ConfigRetriever;
-import io.vertx.core.AbstractVerticle;
+import io.reactivex.Single;
 import io.vertx.core.Future;
+import io.vertx.core.http.HttpServer;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.web.Router;
-import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.client.WebClientOptions;
+import io.vertx.reactivex.config.ConfigRetriever;
+import io.vertx.reactivex.core.AbstractVerticle;
+import io.vertx.reactivex.ext.web.Router;
+import io.vertx.reactivex.ext.web.RoutingContext;
+import io.vertx.reactivex.ext.web.client.WebClient;
+import io.vertx.reactivex.ext.web.codec.BodyCodec;
+import io.vertx.reactivex.servicediscovery.ServiceDiscovery;
+import io.vertx.reactivex.servicediscovery.types.HttpEndpoint;
+import io.vertx.servicediscovery.Record;
+
+
 
 public class InsultGatewayVerticle extends AbstractVerticle {
 	private ConfigRetriever conf;
     private String message;
     private String logLevel;
-
+    private WebClient clientSpringboot;
+    private WebClient clientSwarm;
+    private WebClient clientVertx;
     
     private JsonObject config;
 	
-	
+    private ServiceDiscovery discovery;
 	
 	@Override
     public void start() {
 
 		conf = ConfigRetriever.create(vertx);
-		
-		
 		Router router = Router.router(vertx);
-        router.get("/api/insult").handler(this::retriveInsult);
+		
+
+        clientSpringboot = WebClient.create(vertx, new WebClientOptions()
+                .setDefaultHost("springboot-noun-service.vertx-adjective.svc")
+                .setDefaultPort(8080));
+
+            clientSwarm = WebClient.create(vertx, new WebClientOptions()
+                    .setDefaultHost("wildflyswarm-adj.vertx-adjective.svc")
+                    .setDefaultPort(8080));
+            router.get("/api/insult").handler(this::invoke);
+		
+            ServiceDiscovery.create(vertx, discovery ->
+  	      // Retrieve a web client
+  	      HttpEndpoint.getWebClient(discovery, svc -> svc.getName().equals("vertx-adjective-service"), ar -> {
+  	        if (ar.failed()) {
+  	          System.out.println("D'oh the service is not available");
+  	        } else {
+  	        	clientVertx = ar.result();
+  	        	vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+  	        	
+  	        }
+  	      }));
+		
+		
+        
+		
+		
+		
+		
+		
+		
+        
+        
+            
+            
+        
     	
         retrieveMessageTemplateFromConfiguration()
         .setHandler(ar -> {
@@ -42,6 +87,9 @@ public class InsultGatewayVerticle extends AbstractVerticle {
                     config().getInteger("http.port", 8080));
 
         });
+        
+
+		
         
         retrieveLogLevelFromConfiguration()
         .setHandler(ar ->{
@@ -80,19 +128,42 @@ public class InsultGatewayVerticle extends AbstractVerticle {
         return future;
     }
 
-	
+	private void invoke(RoutingContext rc) {
+		clientVertx.get("/api/adjective").as(BodyCodec.string()).send(ar -> {
+	      if (ar.failed()) {
+	        rc.response().end("Unable to call the greeting service: " + ar.cause().getMessage());
+	      } else {
+	        if (ar.result().statusCode() != 200) {
+	          rc.response().end("Unable to call the greeting service - received status:" + ar.result().statusMessage());
+	        } else {
+	          rc.response().end("Greeting service invoked: '" + ar.result().body() + "'");
+	        }
+	      }
+	    });
+	  }
 	 private void retriveInsult(RoutingContext rc) {
 		 
 		 
 		 if (message == null) {
 	            rc.response().setStatusCode(500)
-	                .putHeader(CONTENT_TYPE, "application/json; charset=utf-8")
+	                .putHeader(CONTENT_TYPE.toString(), "application/json; charset=utf-8")
 	                .end(new JsonObject().put("content", "no config map").encode());
 	            return;
 	        }
 		 
-		
+		 
 		 
 	 }
+		 private Single<Record> publishmsg(HttpServer server) {
+			    // 1 - Create a service record using `io.vertx.reactivex.servicediscovery.types.HttpEndpoint.createRecord`.
+			    // This record defines the service name ("greetings"), the host ("localhost"), the server port and the root ("/")
+			    Record record = HttpEndpoint.createRecord("greetings", "localhost", server.actualPort(), "/");
+
+			    // 2 - Call the rxPublish method with the created record and return the resulting single
+			    return discovery.rxPublish(record);
+			}
+		
+		 
+	 
 
 }
